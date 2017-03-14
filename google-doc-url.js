e26cce68953440525a587a59a3d9b0cb40c8192d
whiteplums <chris.sourire@gmail.com>
2017-03-14 10:40:55 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Qa2FXVetIA-9CQuG4Cry3NLxgLggSAndWrA1I9-7Bp4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Qa2FXVetIA-9CQuG4Cry3NLxgLggSAndWrA1I9-7Bp4/pubhtml';
